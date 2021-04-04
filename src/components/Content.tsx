33c1aa1d7133954aeaa6231845216e063b4cbf5c
Iury Oliveira <iury@rarolabs.com.br>
2021-04-03 21:56:26 -0300

    implementação

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,41 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from './MovieCard';
+
+interface GenreProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+interface ContentProps {
+  selectedGenre: GenreProps;
+  movies: MovieProps[];
+}
+
+export function Content(props: ContentProps) {
+  return(
+    <>
+      <header>
+          <span className="category">Categoria:<span> {props.selectedGenre.title}</span></span>
+        </header>
+
+        <main>
+          <div className="movies-list">
+            {props.movies.map(movie => (
+              <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+            ))}
+          </div>
+        </main>
+    </>
+  )
 }
